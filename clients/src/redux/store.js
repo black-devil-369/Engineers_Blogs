8de6a50066e9f929e44a4f9861d35fc361bb2fe3
black-devil-369 <ajaychauhan1262@gmail.com>
2024-05-03 20:09:01 +0530

    using redux persist

---
+++ clients/src/redux/store.js
@@ -0,0 +1,21 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import useReducer from "./user/userSlice.js";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+const rootRuducer = combineReducers({
+  user: useReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootRuducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+export const persistor = persistStore(store);
