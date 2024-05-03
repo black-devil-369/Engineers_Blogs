3362190881956583dd26a4a6a2451fd5c3bcf1ea
black-devil-369 <ajaychauhan1262@gmail.com>
2024-05-03 19:44:51 +0530

    Sing in page with their functinality

---
+++ clients/src/pages/SignIn.jsx
@@ -0,0 +1,113 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
+function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure("Please fill out all fields"));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
+        {/* left */}
+        <div className="flex-1">
+          <Link to={"/"} className=" font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-500 rounded-xl text-white">
+              Engineer's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Enter your valid Email and Password to Go in the Home Page of this
+            is Blog page for Engineer's
+          </p>
+        </div>
+        {/*right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="youremailxxxx@gmail.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="**********"
+                id="current-password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone={"purpleToPink"}
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size={"sm"} />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="mt-3">
+            <span>Create New Account Here ? </span>
+            <Link to={"/sign-up"} className="text-blue-500">
+              Sing Up
+            </Link>
+            <div>
+              {errorMessage && (
+                <Alert className="mt-5" color="failure">
+                  {errorMessage}
+                </Alert>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
